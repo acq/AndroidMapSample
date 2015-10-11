0ceacc1c80f62ce4395365be30734470f4eca11a
Adrien Couque <adrien.couque@applidium.com>
2015-10-11 17:50:03 +0200

    Add GoogleMaps (through AirMapView)

--- app/src/main/java/com/applidium/paris/activity/MainActivity.java
+++ app/src/main/java/com/applidium/paris/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.applidium.paris.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.airbnb.android.airmapview.AirMapView;
 import com.applidium.paris.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AirMapView mapView = (AirMapView) findViewById(R.id.map);
+        mapView.initialize(getSupportFragmentManager());
     }
 
 }
