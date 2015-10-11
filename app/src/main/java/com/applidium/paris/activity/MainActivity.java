97ee6b812d528d9c18039bf3b0b0e8b9994854f3
Adrien Couque <adrien.couque@applidium.com>
2015-10-11 18:03:07 +0200

    Zoom on Paris

--- app/src/main/java/com/applidium/paris/activity/MainActivity.java
+++ app/src/main/java/com/applidium/paris/activity/MainActivity.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.airbnb.android.airmapview.AirMapView;
+import com.airbnb.android.airmapview.listeners.OnMapInitializedListener;
 import com.applidium.paris.R;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AirMapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AirMapView mapView = (AirMapView) findViewById(R.id.map);
-        mapView.initialize(getSupportFragmentManager());
+        mMapView = (AirMapView) findViewById(R.id.map);
+        mMapView.setOnMapInitializedListener(new OnMapInitializedListener() {
+            @Override
+            public void onMapInitialized() {
+                mMapView.setCenter(new LatLng(48.8567, 2.3508));
+                mMapView.setZoom(12);
+            }
+        });
+        mMapView.initialize(getSupportFragmentManager());
     }
 
 }
