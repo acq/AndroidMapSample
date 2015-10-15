29750bab1a9db8260eafb71d09eb61b0ca618b49
Adrien Couque <ad.couque@gmail.com>
2015-10-15 18:29:31 +0200

    Replace jcenter AirMapView with local dependency

---
+++ external/airmapview/src/main/java/com/airbnb/android/airmapview/GoogleChinaWebViewMapFragment.java
@@ -0,0 +1,7 @@
+package com.airbnb.android.airmapview;
+
+public class GoogleChinaWebViewMapFragment extends GoogleWebViewMapFragment {
+    public static GoogleChinaWebViewMapFragment newInstance(AirMapType mapType) {
+        return (GoogleChinaWebViewMapFragment) new GoogleChinaWebViewMapFragment().setArguments(mapType);
+    }
+}
---
+++ external/airmapview/src/main/java/com/airbnb/android/airmapview/listeners/InfoWindowCreator.java
@@ -0,0 +1,8 @@
+package com.airbnb.android.airmapview.listeners;
+
+import android.view.View;
+
+public interface InfoWindowCreator {
+    // TODO: this should take an AirMapMarker instead of just the ID
+    View createInfoWindow(long id);
+}
