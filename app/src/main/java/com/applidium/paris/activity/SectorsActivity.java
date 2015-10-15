4d1fe86a2dec378df4d9e1aabb3bdc8a6ec68782
Adrien Couque <ad.couque@gmail.com>
2015-10-15 19:06:32 +0200

    Sectors: render geojsons on map

--- app/src/main/java/com/applidium/paris/activity/SectorsActivity.java
+++ app/src/main/java/com/applidium/paris/activity/SectorsActivity.java
@@ -3,8 +3,14 @@ package com.applidium.paris.activity;
 import android.content.Context;
 import android.content.Intent;
 
+import com.airbnb.android.airmapview.NativeGoogleMapFragment;
 import com.applidium.paris.model.Sector;
 import com.applidium.paris.model.SectorProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.maps.android.geojson.GeoJsonLayer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -18,6 +24,14 @@ public class SectorsActivity extends MapActivity {
         super.onMapReady();
 
         List<Sector> sectors = new SectorProvider().getSectors();
-
+        GoogleMap map = ((NativeGoogleMapFragment)mMapView.getMapInterface()).getGoogleMap();
+        for (Sector sector : sectors) {
+            try {
+                GeoJsonLayer layer = new GeoJsonLayer(map, new JSONObject(sector.getGeoJson()));
+                layer.addLayerToMap();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/applidium/paris/model/Sector.java
+++ app/src/main/java/com/applidium/paris/model/Sector.java
@@ -38,4 +38,56 @@ public class Sector extends BaseModel {
     public double surface;
     @Column
     public String geoJson;
+
+    public long getSequentialId() {
+        return sequentialId;
+    }
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public long getInseeNumber() {
+        return inseeNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public int getArrondissement() {
+        return arrondissement;
+    }
+
+    public long getArrondissementId() {
+        return arrondissementId;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getSurface() {
+        return surface;
+    }
+
+    public String getGeoJson() {
+        return geoJson;
+    }
 }
