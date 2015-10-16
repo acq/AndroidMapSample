ceec17dfe02c6b913993cefe0a5b65e095e32c0b
Adrien Couque <ad.couque@gmail.com>
2015-10-16 19:21:37 +0200

    Associate each sector's arrondissement to a color

--- app/src/main/java/com/applidium/paris/activity/SectorsActivity.java
+++ app/src/main/java/com/applidium/paris/activity/SectorsActivity.java
@@ -2,6 +2,7 @@ package com.applidium.paris.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -26,6 +27,8 @@ public class SectorsActivity extends MapListActivity {
     private List<Sector> mSectors;
     private AirMapMarker mSelectionMarker;
 
+    int[][] colors = new int[][]{{240, 163, 255}, {0, 117, 220}, {153, 63, 0}, {76, 0, 92}, {25, 25, 25}, {0, 92, 49}, {43, 206, 72}, {255, 204, 153}, {128, 128, 128}, {148, 255, 181}, {143, 124, 0}, {157, 204, 0}, {194, 0, 136}, {0, 51, 128}, {255, 164, 5}, {255, 168, 187}, {66, 102, 0}, {255, 0, 16}, {94, 241, 242}, {0, 153, 143}, {224, 255, 102}, {116, 10, 255}, {153, 0, 0}, {255, 255, 128}, {255, 255, 0}, {255, 80, 5}};
+
     public static Intent makeIntent(Context context) {
         return new Intent(context, SectorsActivity.class);
     }
@@ -39,7 +42,8 @@ public class SectorsActivity extends MapListActivity {
         for (Sector sector : mSectors) {
             try {
                 GeoJsonLayer layer = new GeoJsonLayer(map, new JSONObject(sector.getGeoJson()));
-                layer.getDefaultPolygonStyle().setFillColor(0x203F00FF);
+                int[] color = colors[sector.getArrondissement()];
+                layer.getDefaultPolygonStyle().setFillColor(Color.argb(0x40, color[0], color[1], color[2]));
                 layer.addLayerToMap();
             } catch (JSONException e) {
                 e.printStackTrace();
