c3a012d3e1cb77b07ff362ff181963c847bbc898
Adrien Couque <adrien.couque@applidium.com>
2015-11-08 20:24:17 +0100

    Remove no longer need external dependency

--- external/airmapview/src/main/java/com/airbnb/android/airmapview/MapType.java
+++
@@ -1,5 +0,0 @@
-package com.airbnb.android.airmapview;
-
-public enum MapType {
-    MAP_TYPE_NORMAL, MAP_TYPE_SATELLITE, MAP_TYPE_TERRAIN
-}
--- external/airmapview/src/main/java/com/airbnb/android/airmapview/listeners/InfoWindowCreator.java
+++
@@ -1,8 +0,0 @@
-package com.airbnb.android.airmapview.listeners;
-
-import android.view.View;
-
-public interface InfoWindowCreator {
-    // TODO: this should take an AirMapMarker instead of just the ID
-    View createInfoWindow(long id);
-}
